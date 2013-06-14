2e4c1591760876f9ad7b3d17916a65e531e084e6
stripe121 <stripe121@gmail.com>
2013-06-14 17:00:00 +0900

    一通り作成

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,11 @@ object ApplicationBuild extends Build {
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
   )
 
+
+  val appDependencies = Seq(
+  "postgresql" % "postgresql" % "8.4-702.jdbc4"
+)
 }
